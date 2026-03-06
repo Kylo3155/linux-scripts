c0b760ce6e9361020549585868120f7fd625fdb2
Kylo <thiagonahuel2016@hotmail.com>
2026-03-05 22:40:54 -0300

    lol digg

--- timer/timer.py
+++ timer/timer.py
@@ -1,58 +1,62 @@
-import subprocess, time, json, os
-
-minutes = 50
+import json
+import os
+import time
+import subprocess
 
 status_file = "/tmp/waybar_timer_status"
+sequence = [50, 10, 40, 10, 30, 10, 20, 10, 10]
+
+def send_notification(actual_phase):
+    type = "Break" if actual_phase % 2 != 0 else "Work"
+    msg = f"phase ended. Next: {type}"
+    subprocess.run(["notify-send", "-u", "critical", "Timer", msg])
+    subprocess.run(["paplay", "/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga"])
 
 def load_status():
     if not os.path.exists(status_file):
-        return {"seconds": minutes*60, "active": False}
+        return {"phase_idx": 0, "remaining": sequence[0] * 60, "active": False, "target": None}
     with open(status_file, "r") as f:
         return json.load(f)
 
-def save_status(status):
-    with open(status_file, "w") as f:
-        json.dump(status, f)
-
 status = load_status()
-save_status(status)
+now = time.time()
+phase = "break" if status["phase_idx"] % 2 != 0 else "work"
 
-if status["active"] and status["seconds"] > 0:
-    status["seconds"] -= 1
-    save_status(status)
+if status["active"]:
+    real_seconds = int(status["target"] - now)
+    
+    if real_seconds <= 0:
+        send_notification(status["phase_idx"])
+        status["phase_idx"] += 1
+        
+        if status["phase_idx"] < len(sequence):
+            new_time = sequence[status["phase_idx"]] * 60
+            status["remaining"] = new_time
+            status["target"] = now + new_time
+            real_seconds = new_time
+        else:
+            status["active"] = False
+            status["phase_idx"] = 0
+            status["remaining"] = sequence[0] * 60
+            real_seconds = status["remaining"]
+    else:
+        status["remaining"] = real_seconds
+else:
+    real_seconds = status["remaining"]
 
-mins = status["seconds"] // 60
-secs = status["seconds"] % 60
+mins, secs = divmod(real_seconds, 60)
 time_text = f"{mins:02d}:{secs:02d}"
 
-icon = "󱎫" if status["active"] else "󱎩"
+is_break = status["phase_idx"] % 2 != 0
+icon = "󱫪" if is_break else "󱫠"
+if not status["active"]: icon = "󱫞"
 
 output = {
     "text": f"{icon} {time_text}",
-    "class": "active" if status["active"] else "paused",
-    "tooltip": "Click to start pause | Right-click to reset"
+    "class": "break" if is_break else "work",
+    "tooltip": f"Phase: {sequence[status["phase_idx"]]} minutes {phase} \nClick to start/pause | Right-click to reset"
 }
 
 print(json.dumps(output))
-
-'''
-def countdown(minutes, stage):
-    seconds = minutes*60
-    while seconds >= 0:
-        #subprocess.run('clear', shell=True)
-        mins, secs = divmod(seconds, 60)
-        #print(f"{mins:02d}:{secs:02d} remaining of {state}")
-        data = {
-            "text": f"{mins:02d}:{secs:02d}, {stage}",
-            "tooltip": f"{mins:02d}:{secs:02d} remaining of {stage}",
-            "class": "custom-python"
-        }
-        #print(json.dumps(data))
-        time.sleep(1)
-        seconds-=1
-
-while(minutes >= 10):
-    countdown(minutes, "work")
-    countdown(10, "break")
-    minutes-=10
-'''
+with open(status_file, "w") as f:
+    json.dump(status, f)
--- timer/toggle_timer.py
+++ timer/toggle_timer.py
@@ -1,10 +1,26 @@
-import json, os 
+import json
+import os
+import time
+import subprocess
 
 status_file = "/tmp/waybar_timer_status"
+sequence = [50, 10, 40, 10, 30, 10, 20, 10, 10]
 
-if os.path.exists(status_file):
+if not os.path.exists(status_file):
+    status = {"phase_idx": 0, "remaining": sequence[0] * 60, "active": False, "target": None}
+else:
     with open(status_file, "r") as f:
         status = json.load(f)
-    status["active"] = not status.get("active", False)
-    with open(status_file, "w") as f:
-        json.dump(status, f)
+
+if status["active"]:
+    status["remaining"] = int(status["target"] - time.time())
+    status["active"] = False
+    status["target"] = None
+else:
+    status["target"] = time.time() + status["remaining"]
+    status["active"] = True
+
+with open(status_file, "w") as f:
+    json.dump(status, f)
+
+subprocess.run(["pkill", "-RTMIN+1", "waybar"])
