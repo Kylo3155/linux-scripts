ea8da67a664ce8a9693b03ab970fe6a07225edaf
Kylo <thiagonahuel2016@hotmail.com>
2026-03-05 23:46:59 -0300

    Glue. Match sticks. Paper. Build script!

--- timer/timer.py
+++ timer/timer.py
@@ -7,10 +7,15 @@ status_file = "/tmp/waybar_timer_status"
 sequence = [50, 10, 40, 10, 30, 10, 20, 10, 10]
 
 def send_notification(actual_phase):
-    type = "Break" if actual_phase % 2 != 0 else "Work"
-    msg = f"phase ended. Next: {type}"
-    subprocess.run(["notify-send", "-u", "critical", "Timer", msg])
-    subprocess.run(["paplay", "/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga"])
+    next_phase_idx = actual_phase + 1
+    if next_phase_idx < len(sequence):
+        next_type = "Break" if next_phase_idx % 2 != 0 else "Work"
+        msg = f"Phase ended. Next: {next_type} ({sequence[next_phase_idx]} min)"
+    else:
+        msg = "All phases completed!"
+        
+    subprocess.Popen(["notify-send", "-u", "critical", "Timer", msg])
+    subprocess.Popen(["paplay", "/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga"])
 
 def load_status():
     if not os.path.exists(status_file):
@@ -26,8 +31,11 @@ if status["active"]:
     real_seconds = int(status["target"] - now)
     
     if real_seconds <= 0:
-        send_notification(status["phase_idx"])
+        
+        old_phase = status["phase_idx"]
         status["phase_idx"] += 1
+
+        send_notification(old_phase)
         
         if status["phase_idx"] < len(sequence):
             new_time = sequence[status["phase_idx"]] * 60
@@ -39,6 +47,10 @@ if status["active"]:
             status["phase_idx"] = 0
             status["remaining"] = sequence[0] * 60
             real_seconds = status["remaining"]
+        
+        with open(status_file, "w") as f:
+            json.dump(status, f)
+
     else:
         status["remaining"] = real_seconds
 else:
